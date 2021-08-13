58b478aa480722cb2aed2002e3f757264e1c4c18
Arsalan Akhtar <arakhtar@icloud.com>
2021-08-14 00:48:36 +0800

    Move urlsessionhttpclient to its own class in production

--- EssentialFeed/FeedApi/URLSessionHTTPClient.swift
+++ EssentialFeed/FeedApi/URLSessionHTTPClient.swift
@@ -9,22 +9,24 @@ import Foundation
 
 public class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
-
+    
     public init(session: URLSession = .shared) {
         self.session = session
     }
-
-    private struct UnexpectedValuesRepresentation: Error {}
-
+    
+    private struct UnexpectedValueRepresentationError: Error { }
+    
     public func get(from url: URL, completion: @escaping (HttpClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
+        //let url = URL(string: "http://wrongurl.com")!
+        session.dataTask(with: url){data,response,error in
             if let error = error {
                 completion(.failure(error))
             } else if let data = data, let response = response as? HTTPURLResponse {
                 completion(.success(data, response))
             } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
+                completion(.failure(UnexpectedValueRepresentationError()))
             }
         }.resume()
     }
+    
 }
--- EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
@@ -8,29 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedValueRepresentationError: Error { }
-    
-    func get(from url: URL, completion: @escaping (HttpClientResult) -> Void) {
-        //let url = URL(string: "http://wrongurl.com")!
-        session.dataTask(with: url){data,response,error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedValueRepresentationError()))
-            }
-        }.resume()
-    }
-    
-}
+
 
 class URLSessionHTTPClientTests: XCTestCase {
     
