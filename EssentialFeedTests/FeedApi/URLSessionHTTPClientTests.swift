036f702f75503b1d18486e80b4d01df6d31a682e
Arsalan Akhtar <arakhtar@icloud.com>
2021-08-24 00:31:30 +0800

    handling race conditions

--- EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
@@ -177,7 +177,6 @@ class URLSessionHTTPClientTests: XCTestCase {
             requestObserver = nil
         }
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
             return true
         }
         
@@ -187,6 +186,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         override func startLoading() {
         
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                requestObserver(request)
+            }
+            
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
